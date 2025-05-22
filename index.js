a681f06e160176bdca40caa14ede38cef39cd2fd
Rochdi Nassah <rochdinassah.1998@gmail.com>
2025-05-22 12:01:33 +0100

    AUTO COMMIT

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, l
   const p = [];
   for (let i = 0, code; 0 > i; ++i) {
     code = rand(111111, 999999);
-    p.push(new Promise(r => httpClient.post(url, headers, 'lsd=AVpc27UC2RQ&n='+code).then(res => (log(res.statusCode), r()))));
+    p.push(new Promise(r => httpClient.post(url, headers, 'lsd=AVpc27UC2BQ&n='+code).then(res => (log(res.statusCode), r()))));
   }
 
   Promise.all(p).then(() => {
